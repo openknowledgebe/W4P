5066abe0f057169de8d2b1a44247d720571ea85e
Nico Verbruggen <nico@underlined.be>
2016-02-05 15:24:23 +0100

    Mails adjusted to conform to same style

--- resources/views/mails/donation_money_success.blade.php
+++ resources/views/mails/donation_money_success.blade.php
@@ -13,12 +13,14 @@
     <p>{!! trans('mails.donation_money_success.content.intro', ['name' => $name, 'project' => $projectTitle, 'amount' => $amount]) !!}</p>
     @if (count($donationContents) > 0)
     <p> {{ trans('mails.donation_money_success.content.additional_pledge') }}</p>
+    <hr/>
     @foreach ($donationContents as $kind => $kindContent)
         <strong>{{ trans('backoffice.' . $kind) }}</strong><br/>
         @foreach ($kindContent as $item)
             {{ $item["count"] }}x {{ $item["name"] }}<br/>
         @endforeach
     @endforeach
+    <hr/>
     <p> {{ trans('mails.donation_money_success.content.additional_pledge_disclaimer') }} </p>
     @endif
     <p> {{ trans('mails.donation_money_success.content.confirm') }}</p>
