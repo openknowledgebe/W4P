d1ba6b9676ab28f1152bbe3d91865b967549fd63
Nico Verbruggen <nico@underlined.be>
2016-02-05 15:20:31 +0100

    Paragraphs instead of breaks in emails

--- resources/lang/en/mails.php
+++ resources/lang/en/mails.php
@@ -5,7 +5,7 @@ return [
         "teaser" => "Your donation has been registered, but it has not been confirmed yet.",
         "content" => [
             "header" => "Please confirm your donation",
-            "intro" => "Hello there, :name.<br/>We have successfully received your donation registration for <strong>:project</strong>. As a part of the donation system, we require donors to confirm their pledge before it is officially counted as collected.<br/> You have pledged the following:",
+            "intro" => "<p>Hello there, :name.</p><p>We have successfully received your donation registration for <strong>:project</strong>.</p><p>As a part of the donation system, we require donors to confirm their pledge before it is officially counted as collected.</p><p>You have pledged the following:</p>",
             "confirm" => "In order to confirm this donation, you can click the following link below:",
             "confirm_action" => "Confirm this donation"
         ],
@@ -16,7 +16,7 @@ return [
         "teaser" => "Your donation has been confirmed.",
         "content" => [
             "header" => "Thanks!",
-            "intro" => "Hello there, :name.<br/>We have successfully received your donation confirmation for <strong>:project</strong>.",
+            "intro" => "<p>Hello there, :name.</p><p>We have successfully received your donation confirmation for <strong>:project</strong>.</p>",
             "confirm" => "In order to view your pledge and your rewards, you can click the following link below:",
             "confirm_action" => "View my pledge"
         ],
@@ -27,7 +27,7 @@ return [
         "teaser" => "Your payment has been completed.",
         "content" => [
             "header" => "Thank your for your donation!",
-            "intro" => "Hello there, :name.<br/>We have successfully received your donation payment (€:amount) for <strong>:project</strong>.",
+            "intro" => "<p>Hello there, :name.</p><p>We have successfully received your donation payment (€:amount) for <strong>:project</strong>.</p>",
             "additional_pledge" => "In addition to this, you have also pledged the following:",
             "additional_pledge_disclaimer" => "Since you already paid for your donation, your pledge was automatically confirmed. The project creator might get in touch with you regarding the items above.",
             "confirm" => "In order to view your pledge and your rewards, you can click the following link below:",
