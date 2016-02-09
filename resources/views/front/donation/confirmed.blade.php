2d7b65a1935a47842b3b890e68a0dc02e18af96b
Nico Verbruggen <nico@underlined.be>
2016-02-09 15:32:44 +0100

    Add share buttons to order confirmation

--- resources/views/front/donation/confirmed.blade.php
+++ resources/views/front/donation/confirmed.blade.php
@@ -18,6 +18,19 @@
                 </div>
                 <div class="col-md-12">
                     <p>{{ trans('donation.confirmed.description') }}</p>
+                    <hr/>
+                    <section class="share">
+                        <h3>{{ trans('home.share') }}</h3>
+                        <a class="share-btn share-fb" href="https://www.facebook.com/sharer/sharer.php?u={{URL::route('home')}}" target="_blank" title="Share on Facebook">
+                            <img src='{{ URL::to('img/share_fb@2x.png') }}' />
+                        </a>
+                        <a class="share-btn share-gp" href="https://plus.google.com/share?url={{URL::route('home')}}" target="_blank" title="Share on Google+">
+                            <img src='{{ URL::to('img/share_gp@2x.png') }}' />
+                        </a>
+                        <a class="share-btn share-tw" href="http://twitter.com/share?text=Check out this project&url={{URL::route('home')}}" target="_blank" title="Share on Twitter">
+                            <img src='{{ URL::to('img/share_tw@2x.png') }}' />
+                        </a>
+                    </section>
                 </div>
             </div>
         </div>
