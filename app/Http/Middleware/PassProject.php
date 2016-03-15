86e58108269684d9db04f7fe1718cc82b531faa1
Nico Verbruggen <nico@underlined.be>
2016-03-15 11:38:01 +0100

    Fix for broken setup

--- app/Http/Middleware/PassProject.php
+++ app/Http/Middleware/PassProject.php
@@ -25,37 +25,40 @@ class PassProject
         // Get the project
         $project = $request->project;
 
-        // Extract the video url
-        $videoId = "";
+        // Only execute this if project is valid or exists
+        if ($project) {
 
-        $videoProvider = null;
-        if (property_exists($project, 'video_url')) {
+            // Extract the video url
+            $videoId = "";
+
+            $videoProvider = null;
+
             if (strpos($project->video_url, 'watch?v=') !== false) {
                 $videoProvider = "youtube";
             }
             if (strpos($project->video_url, 'vimeo.com/') !== false) {
                 $videoProvider = "vimeo";
             }
-        }
 
-        // Check the video provider
-        switch ($videoProvider) {
-            case "vimeo":
-                $array = explode("vimeo.com/", $project->video_url);
-                $videoId = explode("/", last($array))[0];
-                break;
-            case "youtube":
-                $array = explode("watch?v=", $project->video_url);
-                $videoId = explode("&", last($array))[0];
-                break;
-            default:
-                $videoId = null;
-                break;
-        }
+            // Check the video provider
+            switch ($videoProvider) {
+                case "vimeo":
+                    $array = explode("vimeo.com/", $project->video_url);
+                    $videoId = explode("/", last($array))[0];
+                    break;
+                case "youtube":
+                    $array = explode("watch?v=", $project->video_url);
+                    $videoId = explode("&", last($array))[0];
+                    break;
+                default:
+                    $videoId = null;
+                    break;
+            }
 
-        View::share('W4P_project', $request->project);
-        View::share('video_id', $videoId);
-        View::share('video_provider', $videoProvider);
+            View::share('W4P_project', $request->project);
+            View::share('video_id', $videoId);
+            View::share('video_provider', $videoProvider);
+        }
 
         return $next($request);
     }
