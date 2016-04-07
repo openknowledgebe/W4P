c002e182189b04cdb9c956b315c0897432f85d38
Michiel Vancoillie <michiel@underlined.be>
2016-04-07 14:03:07 +0200

    Fix backoffice navbar

--- resources/views/layouts/backoffice.blade.php
+++ resources/views/layouts/backoffice.blade.php
@@ -8,104 +8,104 @@
         <link href='https://fonts.googleapis.com/css?family=Istok+Web:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
     </head>
     <body>
-        <div class="container">
-            <div class="row">
-                <nav class="navbar navbar-inverse">
-                    <div class="container-fluid">
-                        <div class="navbar-header">
-                            <a class="navbar-brand" href="/">{{ \W4P\Models\Setting::get('platform.name') }}</a>
-                            <ul class="nav navbar-nav">
-                                <li>
-                                    <a href="{{ URL::route('admin::index') }}">{{ trans('backoffice.dashboard') }}</a>
-                                </li>
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="/">{{ \W4P\Models\Setting::get('platform.name') }}</a>
+                    <ul class="nav navbar-nav">
+                        <li>
+                            <a href="{{ URL::route('admin::index') }}">{{ trans('backoffice.dashboard') }}</a>
+                        </li>
 
-                                {{-- First dropdown --}}
-                                <li class="dropdown">
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                                        {{ trans('backoffice.manage') }} <span class="caret"></span></a>
-                                    <ul class="dropdown-menu">
-                                        <li class="separator">{{ trans('backoffice.manage_pages') }}</li>
-                                        <li>
-                                            <a href="{{ URL::route('admin::editPage', 'how_it_works') }}">{{ trans('generic.how_does_it_work') }}</a>
-                                        </li>
-                                        <li>
-                                            <a href="{{ URL::route('admin::editPage', 'press') }}">{{ trans('generic.press_materials') }}</a>
-                                        </li>
-                                        <li>
-                                            <a href="{{ URL::route('admin::editPage', 'terms_of_use') }}">{{ trans('generic.terms_of_use') }}</a>
-                                        </li>
-                                        <li>
-                                            <a href="{{ URL::route('admin::editPage', 'privacy_policy') }}">{{ trans('generic.privacy_policy') }}</a>
-                                        </li>
-                                        <li role="separator" class="divider"></li>
-                                        <li class="separator">{{ trans('backoffice.social') }}</li>
-                                        <li>
-                                            <a href="{{ URL::route('admin::social') }}">{{ trans('backoffice.social_settings') }}</a>
-                                        </li>
-                                        <li role="separator" class="divider"></li>
-                                        <li class="separator">{{ trans('backoffice.administration') }}</li>
-                                        <li>
-                                            <a href="{{ URL::route('admin::password') }}">{{ trans('backoffice.change_password') }}</a>
-                                        </li>
-                                        <li>
-                                            <a href="{{ URL::route('admin::previous') }}">{{ trans('backoffice.previous_projects') }}</a>
-                                        </li>
-                                    </ul>
-                                </li>
-
-                                {{-- Second dropdown --}}
-                                <li class="dropdown">
-                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                                        {{ trans('backoffice.project') }} <span class="caret"></span></a>
-                                    <ul class="dropdown-menu">
-                                        <li class="separator">{{ trans('backoffice.manage') }}</li>
-                                        <li>
-                                            <a href="{{ URL::route('admin::project') }}">{{ trans('backoffice.project') }}</a>
-                                        </li>
-                                        <li>
-                                            <a href="{{ URL::route('admin::tiers') }}">{{ trans('backoffice.tiers') }}</a>
-                                        </li>
-                                        <li>
-                                            <a href="{{ URL::route('admin::posts') }}">{{ trans('backoffice.posts') }}</a>
-                                        </li>
-                                        <li>
-                                            <a href="{{ URL::route('admin::goals') }}">{{ trans('backoffice.goals') }}</a>
-                                        </li>
-                                        <li role="separator" class="divider"></li>
-                                        <li class="separator">{{ trans('backoffice.view') }}</li>
-                                        <li>
-                                            <a href="{{ URL::route('admin::donations') }}">{{ trans('backoffice.donations') }}</a>
-                                        </li>
-                                        <li role="separator" class="divider"></li>
-                                        <li class="separator">{{ trans('backoffice.download') }}</li>
-                                        <li>
-                                            <a href="{{ URL::route('admin::userExport') }}">
-                                                {{ trans('backoffice.download_donors') }}
-                                            </a>
-                                        </li>
-                                        <li>
-                                            <a href="{{ URL::route('admin::userExportTiers') }}">
-                                                {{ trans('backoffice.download_tiers') }}
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </li>
-
+                        {{-- First dropdown --}}
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                {{ trans('backoffice.manage') }} <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li class="separator">{{ trans('backoffice.manage_pages') }}</li>
                                 <li>
-                                    <a href="{{ URL::route('admin::organisation') }}">{{ trans('backoffice.organisation') }}</a>
+                                    <a href="{{ URL::route('admin::editPage', 'how_it_works') }}">{{ trans('generic.how_does_it_work') }}</a>
                                 </li>
                                 <li>
-                                    <a href="{{ URL::route('admin::platform') }}">{{ trans('backoffice.platform') }}</a>
+                                    <a href="{{ URL::route('admin::editPage', 'press') }}">{{ trans('generic.press_materials') }}</a>
                                 </li>
                                 <li>
-                                    <a href="{{ URL::route('admin::email') }}">{{ trans('backoffice.email') }}</a>
+                                    <a href="{{ URL::route('admin::editPage', 'terms_of_use') }}">{{ trans('generic.terms_of_use') }}</a>
+                                </li>
+                                <li>
+                                    <a href="{{ URL::route('admin::editPage', 'privacy_policy') }}">{{ trans('generic.privacy_policy') }}</a>
+                                </li>
+                                <li role="separator" class="divider"></li>
+                                <li class="separator">{{ trans('backoffice.social') }}</li>
+                                <li>
+                                    <a href="{{ URL::route('admin::social') }}">{{ trans('backoffice.social_settings') }}</a>
+                                </li>
+                                <li role="separator" class="divider"></li>
+                                <li class="separator">{{ trans('backoffice.administration') }}</li>
+                                <li>
+                                    <a href="{{ URL::route('admin::password') }}">{{ trans('backoffice.change_password') }}</a>
+                                </li>
+                                <li>
+                                    <a href="{{ URL::route('admin::previous') }}">{{ trans('backoffice.previous_projects') }}</a>
                                 </li>
                             </ul>
-                        </div>
-                        <div>
-                        </div>
-                    </div>
-                </nav>
+                        </li>
+
+                        {{-- Second dropdown --}}
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                                {{ trans('backoffice.project') }} <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li class="separator">{{ trans('backoffice.manage') }}</li>
+                                <li>
+                                    <a href="{{ URL::route('admin::project') }}">{{ trans('backoffice.project') }}</a>
+                                </li>
+                                <li>
+                                    <a href="{{ URL::route('admin::tiers') }}">{{ trans('backoffice.tiers') }}</a>
+                                </li>
+                                <li>
+                                    <a href="{{ URL::route('admin::posts') }}">{{ trans('backoffice.posts') }}</a>
+                                </li>
+                                <li>
+                                    <a href="{{ URL::route('admin::goals') }}">{{ trans('backoffice.goals') }}</a>
+                                </li>
+                                <li role="separator" class="divider"></li>
+                                <li class="separator">{{ trans('backoffice.view') }}</li>
+                                <li>
+                                    <a href="{{ URL::route('admin::donations') }}">{{ trans('backoffice.donations') }}</a>
+                                </li>
+                                <li role="separator" class="divider"></li>
+                                <li class="separator">{{ trans('backoffice.download') }}</li>
+                                <li>
+                                    <a href="{{ URL::route('admin::userExport') }}">
+                                        {{ trans('backoffice.download_donors') }}
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="{{ URL::route('admin::userExportTiers') }}">
+                                        {{ trans('backoffice.download_tiers') }}
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li>
+                            <a href="{{ URL::route('admin::organisation') }}">{{ trans('backoffice.organisation') }}</a>
+                        </li>
+                        <li>
+                            <a href="{{ URL::route('admin::platform') }}">{{ trans('backoffice.platform') }}</a>
+                        </li>
+                        <li>
+                            <a href="{{ URL::route('admin::email') }}">{{ trans('backoffice.email') }}</a>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                </div>
+            </div>
+        </nav>
+        <div class="container">
+            <div class="row">
             </div>
             <div class="content">
                 @yield('content')
