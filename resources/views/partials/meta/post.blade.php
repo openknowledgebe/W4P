d12c9ca54281b2a25e260e01008b7221caf14b72
Nico Verbruggen <nico@underlined.be>
2016-05-03 16:12:26 +0200

    OG tag fixes

--- resources/views/partials/meta/post.blade.php
+++ resources/views/partials/meta/post.blade.php
@@ -1,14 +1,14 @@
+<meta name="description" content="{{ $post->summary }}">
 {{-- Facebook OpenGraph --}}
 <meta property="og:url" content="{{ Request::url() }}" />
 <meta property="og:type" content="article" />
 <meta property="og:title" content="{{ $post->title }}" />
 @if ($settings->social->seo_image)
-    <meta name="og:image" content="{{ $settings->social->seo_image }}">
+    <meta property="og:image" content="{{ $settings->social->seo_image }}">
 @else
-    <meta name="og:image" content="{{ URL::to('project/banner.jpg') }}">
+    <meta property="og:image" content="{{ URL::to('project/banner.jpg') }}">
 @endif
-<meta name="og:description" content="{{ $post->summary }}">
-<meta name="description" content="{{ $post->summary }}">
+<meta property="og:description" content="{{ $post->summary }}">
 {{-- Dublin Core --}}
 <link rel="schema.dcterms" href="http://purl.org/dc/terms/">
 <meta name="dcterms.language" content="en">
